f074de1551612f623722ca1765fe0bbed64f32bf
Henry Sullivan <sullivanbhenry@gmail.com>
2022-11-04 09:23:52 -0700

    test passes to add 5 to cost if medium pizza is selected

--- js/scripts.js
+++ js/scripts.js
@@ -6,14 +6,20 @@ function Pizza(toppings, size) {
   this.size = []
 }
 
-Pizza.prototype.findCost = function(toppings, size) {
+Pizza.prototype.findCostSize = function() {
 let cost = 10;
 if (this.size.includes("small")) {
   return cost;
 }else if (this.size.includes("medium")) {
   return cost += 5
 }
+// }else if (this.size.includes("large")) {
+//   return cost += 10
+// }
+// return cost
+}
 
+Pizza.prototype.findCostToppings = function (cost) {
 if (this.toppings.includes("sausage") && this.toppings.includes("pepperoni")) {
   return cost += 3;
 }else if (this.toppings.includes("sausage")) {
@@ -23,3 +29,5 @@ if (this.toppings.includes("sausage") && this.toppings.includes("pepperoni")) {
 }
 
 }
+
+
