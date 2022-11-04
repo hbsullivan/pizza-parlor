e8e007296e54940249adf7dfb57ed4f9fe7efc2e
Henry Sullivan <sullivanbhenry@gmail.com>
2022-11-04 15:38:31 -0700

    review and refactor code for any indentation errors

--- js/scripts.js
+++ js/scripts.js
@@ -2,60 +2,59 @@
 let pizza = new Pizza();
 
 function Pizza(toppings, size) {
-  this.toppings = []
-  this.size = []
+  this.toppings = [];
+  this.size = [];
 }
 
 Pizza.prototype.findCostSize = function() {
-let cost = 10;
-if (this.size.includes("small")) {
+  let cost = 10;
+  if (this.size.includes("small")) {
+    return cost;
+  }else if (this.size.includes("medium")) {
+    return cost += 5;
+  }else if (this.size.includes("large")) {
+    return cost += 10;
+  }
   return cost;
-}else if (this.size.includes("medium")) {
-  return cost += 5
-
-}else if (this.size.includes("large")) {
-  return cost += 10
-}
-return cost
 }
 
 Pizza.prototype.findCostToppings = function (cost) {
-let newCost
-if(this.toppings.includes("sausage") && this.toppings.includes("pepperoni") && this.toppings.includes("veggies")) {
-  let newCost = cost += 3.5;
-  return newCost.toFixed(2)
-}else if (this.toppings.includes("sausage") && this.toppings.includes("pepperoni")){
-  let newCost = cost += 3;
-  return newCost.toFixed(2);
-}else if (this.toppings.includes("sausage") && this.toppings.includes("veggies")){
-  let newCost = cost += 1.5;
-  return newCost.toFixed(2);
-}else if (this.toppings.includes("veggies") && this.toppings.includes("pepperoni")){
-  let newCost = cost += 2.5;
-  return newCost.toFixed(2);
-}else if (this.toppings.includes("veggies")) {
-  let newCost = cost += .5;
-  return newCost.toFixed(2);
-}else if (this.toppings.includes("sausage")) {
-  let newCost = cost += 1;
-  return newCost.toFixed(2);
-}else if (this.toppings.includes("pepperoni")) {
-  let newCost = cost += 2;
-  return newCost.toFixed(2);
-}else if (this.toppings.includes("no-toppings")) {
-  let newCost = cost += 0;
-  return newCost.toFixed(2);
-}
-return newCost;
+  let newCost
+  if(this.toppings.includes("sausage") && this.toppings.includes("pepperoni") && this.toppings.includes("veggies")) {
+    let newCost = cost += 3.5;
+    return newCost.toFixed(2);
+  }else if (this.toppings.includes("sausage") && this.toppings.includes("pepperoni")){
+    let newCost = cost += 3;
+    return newCost.toFixed(2);
+  }else if (this.toppings.includes("sausage") && this.toppings.includes("veggies")){
+    let newCost = cost += 1.5;
+    return newCost.toFixed(2);
+  }else if (this.toppings.includes("veggies") && this.toppings.includes("pepperoni")){
+    let newCost = cost += 2.5;
+    return newCost.toFixed(2);
+  }else if (this.toppings.includes("veggies")) {
+    let newCost = cost += .5;
+    return newCost.toFixed(2);
+  }else if (this.toppings.includes("sausage")) {
+    let newCost = cost += 1;
+    return newCost.toFixed(2);
+  }else if (this.toppings.includes("pepperoni")) {
+    let newCost = cost += 2;
+    return newCost.toFixed(2);
+  }else if (this.toppings.includes("no-toppings")) {
+    let newCost = cost += 0;
+    return newCost.toFixed(2);
+  }
+  return newCost;
 }
 
 //User Interface Logic
 
 
 function getSelected() {
-const selectedSize = document.getElementById("size-option").value
-pizza.size = selectedSize;
-const selectedToppings = document.querySelectorAll("input[name=topping-option]:checked");
+  const selectedSize = document.getElementById("size-option").value;
+  pizza.size = selectedSize;
+  const selectedToppings = document.querySelectorAll("input[name=topping-option]:checked");
   for (var i = 0; i < selectedToppings.length; i++) {
     if (selectedToppings[i].checked) {
       pizza.toppings.push(selectedToppings[i].value);
@@ -68,13 +67,13 @@ const selectedToppings = document.querySelectorAll("input[name=topping-option]:c
 // }
 
 function handleSubmit(event) {
-event.preventDefault();
-getSelected();
-let sizeCost = pizza.findCostSize();
-let finalCost = pizza.findCostToppings(sizeCost);
-document.getElementById("price").innerText = finalCost
-document.getElementById("final-price").removeAttribute("class");
-pizza.toppings = []
+  event.preventDefault();
+  getSelected();
+  let sizeCost = pizza.findCostSize();
+  let finalCost = pizza.findCostToppings(sizeCost);
+  document.getElementById("price").innerText = finalCost;
+  document.getElementById("final-price").removeAttribute("class");
+  pizza.toppings = [];
 } 
 
 function newOrder() {
@@ -83,7 +82,7 @@ function newOrder() {
   const selectedToppings = document.querySelectorAll("input[name=topping-option]:checked");
   for (var i = 0; i < selectedToppings.length; i++) {
     if (selectedToppings[i].checked) {
-        selectedToppings[i].checked = false
+        selectedToppings[i].checked = false;
     } 
   }
 }
@@ -91,5 +90,5 @@ function newOrder() {
 
 window.addEventListener("load", function(){
   document.getElementById("order-form").addEventListener("submit", handleSubmit);
-  document.getElementById("new-order").addEventListener("click", newOrder)
+  document.getElementById("new-order").addEventListener("click", newOrder);
 })
